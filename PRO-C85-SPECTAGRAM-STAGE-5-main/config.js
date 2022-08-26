f2d086038322bd215eb98049bfc0c0a4ba376581
johanjince12 <96847741+johanjince12@users.noreply.github.com>
2022-08-26 18:00:09 -0500

    Add files via upload

---
+++ PRO-C85-SPECTAGRAM-STAGE-5-main/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/app';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBKTCNNkirzI5_iplvzshdS5NOXLuok1ao",
+  authDomain: "spectagram-29f43.firebaseapp.com",
+  databaseURL: "https://spectagram-29f43-default-rtdb.firebaseio.com",
+  projectId: "spectagram-29f43",
+  storageBucket: "spectagram-29f43.appspot.com",
+  messagingSenderId: "215043925395",
+  appId: "1:215043925395:web:008779f04379036e617466"
+};
